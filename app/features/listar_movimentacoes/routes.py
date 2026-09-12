44fe96cdc339968871aba71b947ba635c68e4864
Gabriel Martins Cirino <gabrielcirinom@dinfor.uniceub>
2026-09-11 21:28:53 -0300

    Corrie erro de Lint

--- app/features/listar_movimentacoes/routes.py
+++ app/features/listar_movimentacoes/routes.py
@@ -2,7 +2,12 @@ from flask import Blueprint, jsonify, render_template
 
 from app.features.listar_movimentacoes.service import status
 
-bp = Blueprint("listar_movimentacoes", __name__, url_prefix="/listar-movimentacoes", template_folder="templates")
+bp = Blueprint(
+    "listar_movimentacoes", 
+    __name__,
+     url_prefix="/listar-movimentacoes",
+      template_folder="templates",
+)
 
 
 @bp.get("")
