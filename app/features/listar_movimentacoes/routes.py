06fc063535d89e6ee0cd8315528ffc4a531f675e
rezelay <layara.rezende@sempreceub.com>
2026-09-25 18:08:12 -0300

    feat: implementa funcionalidade da issue 85

---
+++ app/features/listar_movimentacoes/routes.py
@@ -0,0 +1,20 @@
+from flask import Blueprint, jsonify, render_template
+
+from app.features.listar_movimentacoes.service import status
+
+bp = Blueprint(
+    "listar_movimentacoes",
+    __name__,
+    url_prefix="/listar-movimentacoes",
+    template_folder="templates"
+)
+
+
+@bp.get("")
+def page():
+    return render_template("listar-movimentacoes.html")
+
+
+@bp.get("/api")
+def api():
+    return jsonify(status())
