06e18936cc9c80fdbf944af865391bf4c9b20486
Eduardo Cantuaria Clemente <eduardo.clemente@anater.org>
2026-09-25 13:41:58 -0300

    fix: corrige formatação do ruff e ajusta estrutura

--- app/features/bloquear_quantidade/routes.py
+++ app/features/bloquear_quantidade/routes.py
@@ -2,8 +2,12 @@ from flask import Blueprint, jsonify, render_template
 
 from app.features.bloquear_quantidade.service import status
 
-bp = Blueprint("bloquear_quantidade", __name__, url_prefix="/bloquear-quantidade", template_folder="templates")
-
+bp = Blueprint(
+    "bloquear_quantidade",
+    __name__,
+    url_prefix="/bloquear-quantidade",
+    template_folder="templates",
+)
 
 @bp.get("")
 def page():
